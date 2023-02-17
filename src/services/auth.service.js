928124d6fda2038afca0e6bde79c7930bbc268aa
Eric Pereira <eric.pereira@hotmail.com>
2023-02-17 19:01:19 -0300

    use dotenv

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5555";
+const API_URL = process.env.REACT_APP_BASE_URL;
 
 const register = (name, email, password) => {
   return axios.post(API_URL + '/users', {
--- src/services/post.service.js
+++ src/services/post.service.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import authHeader from "./auth-header";
 
-const API_URL = "http://localhost:5555";
+const API_URL = process.env.REACT_APP_BASE_URL;
 
 const getAllPosts = () => {
     return axios.get(API_URL + '/posts', { headers: authHeader() });
