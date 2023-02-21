f9f15008a287cdbf2c0cf4f998a2d3ae1b75eb12
Eric Pereira <eric.pereira@hotmail.com>
2023-02-21 12:13:38 -0300

    remove redux dev tools

--- src/store.js
+++ src/store.js
@@ -1,5 +1,4 @@
 import { createStore, applyMiddleware } from "redux";
-import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers";
 
@@ -7,7 +6,7 @@ const middleware = [thunk];
 
 const store = createStore(
   rootReducer,
-  composeWithDevTools(applyMiddleware(...middleware))
+  applyMiddleware(...middleware)
 );
 
 export default store;
